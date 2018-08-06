1e0a68bf70d1a3d72110c5e8f3a4874a4d2ece86
velizjuan96 <juancarlosveliz96@hotmail.com>
2018-08-06 17:39:36 -0600

    proyecto

---
+++ dist/conexion/conexion.php
@@ -0,0 +1,6 @@
+<?php
+$conexion = new PDO("mysql:host=127.0.0.1;dbname=eladeria", "root", "");
+$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+?>
---
+++ dist/controladores/editar_ventas.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: juAN
+ * Date: 03/08/2018
+ * Time: 15:19
+ */
